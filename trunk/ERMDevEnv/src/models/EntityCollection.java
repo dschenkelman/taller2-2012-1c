756ea4b8e778d8e3e3030d0bf7ef71e7d16c34d3
santi2332 <santi2332@5ae3dd35-032c-b900-f1dd-ff85be71a015>
2012-04-06 20:31:51 +0000

    se agrega ModelCollection, cambios entityCollection y se agrega attribute

--- trunk/ERMDevEnv/src/models/EntityCollection.java
+++ trunk/ERMDevEnv/src/models/EntityCollection.java
@@ -6,45 +6,27 @@ import infrastructure.IterableExtensions;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EntityCollection {
-	private List<Entity> entities;
-
-	public EntityCollection() {
-		this.entities = new ArrayList<Entity>();
+public class EntityCollection extends ModelCollection<Entity>{
+	
+	public EntityCollection () {
+		super ();
 	}
-
-	public int count() {
-		return this.entities.size();
-	}
-
-	public boolean add(String entityName) {
-		return this.add(entityName, EntityType.None);
-	}
-
+	
+	
 	public boolean add(String entityName, EntityType entityType) {
 		if (this.get(entityName) == null) 
 		{
 			Entity entity = new Entity(entityName);
 			entity.setType(entityType);
-			this.entities.add(entity);
+			this.items.add(entity);
 			return true;
 		}
 		return false;
 	}
 	
-	public Entity get(String entityName) {
-		return IterableExtensions.firstOrDefault(this.entities,
-				new Func<Entity, String, Boolean>() {
-					@Override
-					public Boolean execute(Entity entity, String entityName) {
-						return entity.getName() == entityName;
-					}
-				}, entityName);
-	}	
-
-	public boolean remove(String entityName) {
-		Entity entity = this.get(entityName);
-		return this.entities.remove(entity);
+	@Override
+	public Entity createItemInstance (String entityName) {
+		return new Entity(entityName);
 	}
-
+		
 }
