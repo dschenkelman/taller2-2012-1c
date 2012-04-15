09caceff4e8de174c0cacf540ff374f77b287de1
gdfesta@gmail.com <gdfesta@gmail.com@5ae3dd35-032c-b900-f1dd-ff85be71a015>
2012-04-15 21:49:24 +0000

    interface to notify when a entity is created

---
+++ ERMDevEnv/src/controllers/EntityCreatedListener.java
@@ -0,0 +1,8 @@
+package controllers;
+
+import models.Entity;
+
+public interface EntityCreatedListener {
+
+    public void entityCreated(Entity entity);
+}
--- ERMDevEnv/src/controllers/IEntityController.java
+++ ERMDevEnv/src/controllers/IEntityController.java
@@ -1,7 +1,17 @@
 package controllers;
 
 import models.Entity;
+import views.IEntityView;
 
 public interface IEntityController {
-	Entity create();
+
+    public Entity create(); //should be public void create()
+
+    public void addSubscriber(EntityCreatedListener listener);
+
+    public boolean addEntity();
+
+    public boolean validateEntityName(String name);
+
+    public void setEntityView(IEntityView entityView);
 }
