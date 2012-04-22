673eb5b72abb4fa14cb7ea600f3923b2d65a69a7
gdfesta@gmail.com <gdfesta@gmail.com@5ae3dd35-032c-b900-f1dd-ff85be71a015>
2012-04-22 14:16:01 +0000

    mis errores anteriores

--- trunk/ERMDevEnv/src/views/IKeysView.java
+++ trunk/ERMDevEnv/src/views/IKeysView.java
@@ -1,5 +1,6 @@
 package views;
 
+import controllers.IKeysController;
 import models.IKey;
 import controllers.KeysController;
 
@@ -9,7 +10,7 @@ import java.util.List;
 public interface IKeysView {
     void show(Iterable<IKey> possibleKeys);
 
-    void setController(KeysController controller);
+    void setController(IKeysController controller);
 
     HashMap<Integer,List<IKey>> getKeys();
 }
