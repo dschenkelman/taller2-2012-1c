999f78c6399392f44e54f893187bb8e42c221ff0
santi2332 <santi2332@5ae3dd35-032c-b900-f1dd-ff85be71a015>
2012-04-25 04:29:04 +0000

    cambios Attribute,attributeCollectionXmlManager

--- trunk/ERMDevEnv/src/persistence/AttributeCollectionXmlManager.java
+++ trunk/ERMDevEnv/src/persistence/AttributeCollectionXmlManager.java
@@ -18,110 +18,47 @@ import org.w3c.dom.Text;
 
 public class AttributeCollectionXmlManager {
 
-	private static String ATTRIBUTES = "attributes";
-	private static String ATTRIBUTE = "attribute";
-	private static String IDGROUPS = "idGroups";
-	private static String NAME = "name";
-	private static String TYPE = "type";
-	private static String EXPRESSION = "expression";
-	private static String MINCARDINALITY  = "minimumCardinality";
-	private static String MAXCARDINALITY = "maximumCardinality";
-	private static String OWNERID  = "ownerId";
-	private static String ID = "id";
-	private static String TRUE = "true";
-	private static String FALSE = "false";
-	
-
-	
-	public static Element getAttributesElementFromHierarchyCollection(
-			AttributeCollection attCol, Document document) {
+	public static AttributeCollection getItemFromXmlElement(Element attributesElem){
 		
-		Element attributesElement = document.createElement(ATTRIBUTES);
-
-        Iterator<Attribute> attributesIte = attCol.iterator();
-       while(attributesIte.hasNext()) {
-    	   Attribute attribute = attributesIte.next();
-    	   Node attributeNode = createAttributeNode(attribute, document);
-           attributesElement.appendChild(attributeNode);
-        }
-
-		return attributesElement;
+		
+		return null;
 	}
 
-
+	public static Element getXMLElementFromItem (AttributeCollection attCol, Document doc) {
+		Element attColElement = doc.createElement("attributes");
+		Iterator<Attribute> ite = attCol.iterator();
+		while (ite.hasNext()) {
+			Attribute att = ite.next();
+			attColElement.appendChild(AttributeCollectionXmlManager.createAttributeNode(att,doc));
+		}		
+		return attColElement;
+	}
+	
 
 	private static Node createAttributeNode(Attribute attribute,
 			Document document) {
-		 Element attributeElement = document.createElement(ATTRIBUTES);
+		 Element attributeElement = document.createElement("attribute");
+		 if (attribute.isComposite()) {
+			 attributeElement.appendChild(AttributeCollectionXmlManager.getXMLElementFromItem(attribute.getAttributes()
+					 , document));
+		 }
+		 if (attribute.getIdGroup()!= null) {
+			 attributeElement.appendChild(IdGroupCollectionXmlManager.getIdGroupCollectionToAttribute(attribute.getIdGroup(),
+					 document));
+		 }
 		 
-		 
-		 
-		return null;
+		 //seteo todos los atributos del elemento Attribute
+		 attributeElement.setAttribute("name", attribute.getName().toString());
+		 if (attribute.getType()!=null)   attributeElement.setAttribute("type", attribute.getType().toString());
+		 if (attribute.getExpression()!=null) attributeElement.setAttribute("expression",attribute.getExpression().toString());
+		 if (attribute.getCardinality()!=null) {
+			 attributeElement.setAttribute("minimumCardinality",String.valueOf(attribute.getCardinality().getMinimum()));
+			 attributeElement.setAttribute("maximumCardinality",String.valueOf(attribute.getCardinality().getMaximum()));
+		 }
+		 if (attribute.getOwnerId() != null) attributeElement.setAttribute("ownerId",attribute.getOwnerId().toString());
+		 attributeElement.setAttribute("id",attribute.getId().toString());
+		return attributeElement;
 	}
-	
-	
-    private static Node createHierarchyNode(Attribute att, Document document) {
-       
-
-        
-
-        Attr totalAttribute = document.createAttribute(TOTALATTRIBUTE);
-        totalAttribute.setValue(hierarchy.isTotal() ? TRUE : FALSE);
-        hierarchyElement.setAttributeNode(totalAttribute);
-
-        Attr exclusiveAttribute = document.createAttribute(EXCLUSIVEATTRIBUTE);
-        exclusiveAttribute.setValue(hierarchy.isExclusive() ? TRUE : FALSE);
-        hierarchyElement.setAttributeNode(exclusiveAttribute);
-
-        Attr idAttribute = document.createAttribute(IDATTRIBUTE);
-        idAttribute.setValue(hierarchy.getUUID().toString());
-        hierarchyElement.setAttributeNode(idAttribute);
-
-        Element specificEntitiesElement = document.createElement(SPECIFICENTITIES);
-        Iterable<UUID> specificEntities = hierarchy.getChildren();
-        for (UUID entityChild : specificEntities) {
-            Node hierarchyNode = createSpecificEntitiesNode(entityChild, document);
-            specificEntitiesElement.appendChild(hierarchyNode);
-        }
-
-        hierarchyElement.appendChild(specificEntitiesElement);
-        return hierarchyElement;
-    }
-
-    private static Node createSpecificEntitiesNode(UUID childHierarchy, Document document) {
-        Element entityIdElement = document.createElement(ENTITYID);
-        entityIdElement.appendChild(document.createTextNode(childHierarchy.toString()));
-        return entityIdElement;
-    }
-
-    private static void addHierarchiesToHierarchyCollection(NodeList hierarchies, HierarchyCollection hierarchyCollection) {
-
-        for (int i = 0; i < hierarchies.getLength(); i++) {
-            Element hierarchyElement = (Element) hierarchies.item(i);
-
-            boolean exclusive = hierarchyElement.getAttribute(EXCLUSIVEATTRIBUTE).equals(TRUE);
-            boolean total = hierarchyElement.getAttribute(TOTALATTRIBUTE).equals(TRUE);
-            UUID generalEntityUUID = UUID.fromString(hierarchyElement.getAttribute(GENERALENTITYATTRIBUTE));
-            UUID hierarchyId = UUID.fromString(hierarchyElement.getAttribute(IDATTRIBUTE));
-            ArrayList<UUID> entityIds = new ArrayList<UUID>();
-
-            NodeList specificsHierarchies = hierarchyElement.getElementsByTagName(SPECIFICENTITIES);
-            if (specificsHierarchies.getLength() != 0) {
-                NodeList entityIdsElement = ((Element) specificsHierarchies.item(0)).getElementsByTagName(ENTITYID);
-                for (int j = 0; j < entityIdsElement.getLength(); j++) {
-                    Text id = (Text) (entityIdsElement.item(j)).getFirstChild();
-                    entityIds.add(UUID.fromString(id.getNodeValue()));
-                }
-            }
-
-            try {
-                hierarchyCollection.createHierarchy(hierarchyId, generalEntityUUID, exclusive, total, entityIds);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-	
 	
 
 }
