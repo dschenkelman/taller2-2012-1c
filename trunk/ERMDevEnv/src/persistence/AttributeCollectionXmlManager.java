e34c4e7da0eddcc2cc4a3ec51ccc88ee9b468603
gdfesta@gmail.com <gdfesta@gmail.com@5ae3dd35-032c-b900-f1dd-ff85be71a015>
2012-05-05 23:51:14 +0000

    attribute xml manager, cambie por la recursividad que hablamos el otro dia con santi

--- trunk/ERMDevEnv/src/persistence/AttributeCollectionXmlManager.java
+++ trunk/ERMDevEnv/src/persistence/AttributeCollectionXmlManager.java
@@ -14,99 +14,105 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-public class AttributeCollectionXmlManager implements IXmlManager<AttributeCollection>{
+public class AttributeCollectionXmlManager implements IXmlManager<AttributeCollection> {
 
-	public  AttributeCollection getItemFromXmlElement(Element attributesElem) throws NumberFormatException, Exception{
+    public AttributeCollection getItemFromXmlElement(Element attributesElem) throws NumberFormatException, Exception {
 
-		AttributeCollection attCollection  = new AttributeCollection();
-		if (attributesElem == null)
-			return attCollection;
-		for (int i = 0 ; i < attributesElem.getChildNodes().getLength() ; i++) {
-	
-			
-			Element attribute  = (Element)attributesElem.getChildNodes().item(i) ;
-										
-			String name  = attribute.getAttribute("name");
-			boolean isKeyField = Boolean.valueOf(attribute.getAttribute("isKeyField"));
-			Cardinality cardinality = null;
-			if (!attribute.getAttribute("minimumCardinality").isEmpty() && !attribute.getAttribute("maximumCardinality").isEmpty()) {
-				cardinality  = new Cardinality (Double.valueOf(attribute.getAttribute("minimumCardinality")),
-					Double.valueOf(attribute.getAttribute("maximumCardinality")));
-			}
-			
-			String expression  = null;
-			if (!attribute.getAttribute("expression").isEmpty())
-				expression = attribute.getAttribute("expression");
-			
-			AttributeType type = null;
-			if (!attribute.getAttribute("type").isEmpty())
-				type = AttributeType.valueOf(attribute.getAttribute("type"));
-			
-			
-			IdGroupCollection idGroup = null;
-			
-			if (attribute.getChildNodes().getLength()>0 && 
-					(attribute.getChildNodes().item(0).getNodeName().equals("idGroups") ||
-					attribute.getChildNodes().item(1).getNodeName().equals("idGroups"))  ){
-			
-							idGroup = new IdGroupCollectionXmlManager().getItemFromXmlElement((Element)attribute);
-			}
-			
-			AttributeCollection attrCol = null;
-			
+        AttributeCollection attCollection = new AttributeCollection();
+        if (attributesElem == null)
+            return attCollection;
+        for (int i = 0; i < attributesElem.getChildNodes().getLength(); i++) {
 
-			if (attribute.getChildNodes().getLength()>0 && 
-					attribute.getChildNodes().item(0).getNodeName().equals("attributes"))
-				attrCol = getItemFromXmlElement((Element)attribute.getChildNodes().item(0));
-			
-			UUID myID = UUID.fromString(attribute.getAttribute("id"));
 
-			attCollection.addAttribute(new Attribute(name,isKeyField,cardinality,idGroup,type,expression,attrCol,myID));
-			
-		}
-		
-		return attCollection;
-	}
+            Element attribute = (Element) attributesElem.getChildNodes().item(i);
 
-	
-	public Element getElementFromItem (AttributeCollection attCol, Document doc) {
-		Element attColElement = doc.createElement("attributes");
-		
-		Iterator<Attribute> ite = attCol.iterator();
-		
-		while (ite.hasNext()) {
-			Attribute att = ite.next();
-			attColElement.appendChild(AttributeCollectionXmlManager.createAttributeNode(att,doc));
-		}		
-		return attColElement;
-	}
-	
+            String name = attribute.getAttribute("name");
+            boolean isKeyField = Boolean.valueOf(attribute.getAttribute("isKeyField"));
+            Cardinality cardinality = null;
+            if (!attribute.getAttribute("minimumCardinality").isEmpty() && !attribute.getAttribute("maximumCardinality").isEmpty()) {
+                cardinality = new Cardinality(Double.valueOf(attribute.getAttribute("minimumCardinality")),
+                        Double.valueOf(attribute.getAttribute("maximumCardinality")));
+            }
 
-	private static Node createAttributeNode(Attribute attribute,
-			Document document) {
-		 Element attributeElement = document.createElement("attribute");
-		 if (attribute.getAttributes()!= null) {
-			 attributeElement.appendChild(new AttributeCollectionXmlManager().getElementFromItem(attribute.getAttributes()
-					 , document));
-		 }
-		 if (attribute.getIdGroup()!= null) {
-			 attributeElement.appendChild(new IdGroupCollectionXmlManager().getElementFromItem(attribute.getIdGroup(),
-					 document));
-		 }
-		 
-		 //seteo todos los atributos del elemento Attribute
-		 attributeElement.setAttribute("name", attribute.getName().toString());
-		 if (attribute.getType()!=null)   attributeElement.setAttribute("type", attribute.getType().toString());
-		 if ((attribute.getType() == AttributeType.calculated || attribute.getType() == AttributeType.copy) && attribute.getExpression() != null ) 
-			 attributeElement.setAttribute("expression",attribute.getExpression().toString());
-		 if (attribute.getCardinality()!=null) {
-			 attributeElement.setAttribute("minimumCardinality",String.valueOf(attribute.getCardinality().getMinimum()));
-			 attributeElement.setAttribute("maximumCardinality",String.valueOf(attribute.getCardinality().getMaximum()));
-		 }
-		 attributeElement.setAttribute("id",attribute.getId().toString());
-		 attributeElement.setAttribute("isKeyField", Boolean.toString(attribute.isKey()));
-		return attributeElement;
-	}
-	
+            String expression = null;
+            if (!attribute.getAttribute("expression").isEmpty())
+                expression = attribute.getAttribute("expression");
+
+            AttributeType type = null;
+            if (!attribute.getAttribute("type").isEmpty())
+                type = AttributeType.valueOf(attribute.getAttribute("type"));
+
+
+            IdGroupCollection idGroup = null;
+
+            if (attribute.getChildNodes().getLength() > 0) {
+
+                if (attribute.getChildNodes().item(0).getNodeName().equals("idGroups")) {
+
+                    idGroup = new IdGroupCollectionXmlManager().getItemFromXmlElement((Element) attribute.getChildNodes().item(0));
+
+                } else if (attribute.getChildNodes().item(1).getNodeName().equals("idGroups")) {
+
+                    idGroup = new IdGroupCollectionXmlManager().getItemFromXmlElement((Element) attribute.getChildNodes().item(1));
+
+                }
+            }
+
+            AttributeCollection attrCol = null;
+
+
+            if (attribute.getChildNodes().getLength() > 0 &&
+                    attribute.getChildNodes().item(0).getNodeName().equals("attributes"))
+                attrCol = getItemFromXmlElement((Element) attribute.getChildNodes().item(0));
+
+            UUID myID = UUID.fromString(attribute.getAttribute("id"));
+
+            attCollection.addAttribute(new Attribute(name, isKeyField, cardinality, idGroup, type, expression, attrCol, myID));
+
+        }
+
+        return attCollection;
+    }
+
+
+    public Element getElementFromItem(AttributeCollection attCol, Document doc) {
+        Element attColElement = doc.createElement("attributes");
+
+        Iterator<Attribute> ite = attCol.iterator();
+
+        while (ite.hasNext()) {
+            Attribute att = ite.next();
+            attColElement.appendChild(AttributeCollectionXmlManager.createAttributeNode(att, doc));
+        }
+        return attColElement;
+    }
+
+
+    private static Node createAttributeNode(Attribute attribute,
+                                            Document document) {
+        Element attributeElement = document.createElement("attribute");
+        if (attribute.getAttributes() != null) {
+            attributeElement.appendChild(new AttributeCollectionXmlManager().getElementFromItem(attribute.getAttributes()
+                    , document));
+        }
+        if (attribute.getIdGroup() != null) {
+            attributeElement.appendChild(new IdGroupCollectionXmlManager().getElementFromItem(attribute.getIdGroup(),
+                    document));
+        }
+
+        //seteo todos los atributos del elemento Attribute
+        attributeElement.setAttribute("name", attribute.getName().toString());
+        if (attribute.getType() != null) attributeElement.setAttribute("type", attribute.getType().toString());
+        if ((attribute.getType() == AttributeType.calculated || attribute.getType() == AttributeType.copy) && attribute.getExpression() != null)
+            attributeElement.setAttribute("expression", attribute.getExpression().toString());
+        if (attribute.getCardinality() != null) {
+            attributeElement.setAttribute("minimumCardinality", String.valueOf(attribute.getCardinality().getMinimum()));
+            attributeElement.setAttribute("maximumCardinality", String.valueOf(attribute.getCardinality().getMaximum()));
+        }
+        attributeElement.setAttribute("id", attribute.getId().toString());
+        attributeElement.setAttribute("isKeyField", Boolean.toString(attribute.isKey()));
+        return attributeElement;
+    }
+
 
 }
