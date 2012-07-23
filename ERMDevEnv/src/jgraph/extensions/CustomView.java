fc2921a82f60f0b415f759b59ca3972adc4df806
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@5ae3dd35-032c-b900-f1dd-ff85be71a015>
2012-07-23 00:01:59 +0000

    # Updated DiagramController and CustomView to aim weak entity link to the begginig of the attribute chain

--- ERMDevEnv/src/jgraph/extensions/CustomView.java
+++ ERMDevEnv/src/jgraph/extensions/CustomView.java
@@ -28,15 +28,29 @@ public class CustomView extends mxGraphView {
 				(source) ? mxConstants.STYLE_SOURCE_PERIMETER_SPACING
 						: mxConstants.STYLE_TARGET_PERIMETER_SPACING);
 		
-		if (((mxCell)edge.getCell()).getStyle().equalsIgnoreCase(StyleConstants.NON_FIRST_ID_GROUP_CONNECTOR_STYLE) && 
-				((mxCell)start.getCell()).getStyle().equalsIgnoreCase(StyleConstants.FIRST_ID_GROUP_CONNECTOR_STYLE)){
+		if (this.shouldUseEndPoint(edge, start)){
 			edge.setAbsoluteTerminalPoint(start.getAbsolutePoint(start.getAbsolutePointCount() -1 ), source);
+			return;
+		}
+		
+		if (this.shouldUseStartPoint(edge, start)){
+			edge.setAbsoluteTerminalPoint(start.getAbsolutePoint(0), source);
 			return;
 		}
 		
 		mxPoint pt = getPerimeterPoint(start, next, graph.isOrthogonal(edge),
 				border);
 		edge.setAbsoluteTerminalPoint(pt, source);
+	}
+
+	private boolean shouldUseEndPoint(mxCellState edge, mxCellState start) {
+		return (((mxCell)edge.getCell()).getStyle().equalsIgnoreCase(StyleConstants.NON_FIRST_ID_GROUP_CONNECTOR_STYLE) &&
+				((mxCell)start.getCell()).getStyle().equalsIgnoreCase(StyleConstants.FIRST_ID_GROUP_CONNECTOR_STYLE));
+	}
+	
+	private boolean shouldUseStartPoint(mxCellState edge, mxCellState end) {
+		return (((mxCell)edge.getCell()).getStyle().equalsIgnoreCase(StyleConstants.WEAK_ENTITY_CONNECTOR_STYLE) &&
+				((mxCell)end.getCell()).getStyle().equalsIgnoreCase(StyleConstants.FIRST_ID_GROUP_CONNECTOR_STYLE));
 	}
 }
 
